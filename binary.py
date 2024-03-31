b9044c3d50aeb8446ecdbe95d4a642523bb9240d
stephojuyah <165149570+stephojuyah@users.noreply.github.com>
2024-03-31 12:40:58 +0100

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,40 +1,97 @@
-def binary_calc():
-    def dec_to_base():
-       
-        print("A: base-2 ")
-        print("B: base-8 ")
-        print("C: base-16 ")
+def dec_to_base():
+
+    print("A: Binary ")
+    print("B: Octal ")
+    print("C: Hexa ")
+
+    base = input("Enter Base: ").lower()
+    decimal = int(input("Enter Decimal "))
+
+# Convert decimal to binary        
+    if base == "a":
+        binary_num =  bin(decimal)[2:]
+        print("Binary: ",binary_num)
+
+
+# Convert decimal to octal
+    if base == "b":
+        octal_number = oct(decimal)[2:]
+        print("Octal: ",octal_number)
+
+
+
+# Convert decimal to hexadecimal
+    if base == "c":
+        hexadec =  hex(decimal)[2:]
+        print("Hexadecimal: ",hexadec)
         
-        base = input("Enter base ").lower()
-        decimal = int(input("Enter decimal "))
-        
-    # Convert decimal to binary        
-        if base == "a":
-            binary_num =  bin(decimal)[2:]
-            print("Binary: ",binary_num)
-            
-        
-    # Convert decimal to octal
-        if base == "b":
-            octal_number = oct(decimal)[2:]
-            print("Octal: ",octal_number)
-            
-            
-        
-    # Convert decimal to hexadecimal
-        if base == "c":
-            hexadec =  hex(decimal)[2:]
-            print("Hexadecimal: ",hexadec)
-            
+def bin_to_base():
+    print("A: Decimal ")
+    print("B: Octal ")
+    print("C: Hexa ")
+
+    base = input("Enter Base: ").lower()
+    binary = input("Enter Binary number: ")
+
+    # Convert binary to decimal
+    if base == "a":
+    	decimal = int(str(binary), 2)
+    	print("Decimal: ", decimal)
     
-    print("Conversion Of Bases")
-    
-    print("1: Convert from decimal to other base ")
-    print("2: Convert from other base  to decimal ")
-    print("3: convert from base-n to other base ")
-    choice = int(input("choose conversion" ))
-    
-    if choice == 1: 
-        dec_to_base()
-    if choice != 1:
-        print("not created yet")
+
+    # Convert binary to octal
+    if base == "b":
+        octal_number = oct(decimal)[2:]
+        print("Octal: ", octal_number)
+
+    # Convert binary to hexadecimal
+    if base == "c":
+        hexadec = hex(decimal)[2:]
+        print("Hexadecimal: ", hexadec)
+        
+def oct_to_base():
+
+    print("A: Binary ")
+    print("B: Decimal ")
+    print("C: Hexa ")
+
+    base = input("Enter Base: ").lower()
+    octal = input("Enter Octal number: ")
+
+    # Convert octal to binary
+    if base == "a":
+        binary_num = bin(decimal)[2:]
+        print("Binary: ", binary_num)
+
+    # Convert octal to decimal
+    if base == "b":
+        decimal = int(octal, 8)
+        print("Decimal: ", decimal)
+
+    # Convert octal to hexadecimal
+    if base == "c":
+        hexadec = hex(decimal)[2:]
+        print("Hexadecimal: ", hexadec)
+
+
+print("Conversion of Bases")
+print("========================")
+print("")
+print("1: Decimal to Other Bases ")
+print("2: Binary to Other Bases")
+print("3: Octal to Other Bases ")
+print("4: Hexa to Other Bases")
+print("5: Exit")
+print("")
+print("========================")
+choice = int(input("Choose Operation: " ))
+
+
+
+	
+if choice == 1: 
+    dec_to_base()
+if choice == 2:
+    bin_to_base()
+if choice == 3:
+	oct_to_base()
