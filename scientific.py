0de58f33a598ea1760654a14babb9924e3a61bf5
T-boyzcode <165224141+T-boyzcode@users.noreply.github.com>
2024-03-31 18:09:32 +0100

    Update scientific.py
    I created functions for scientific calculations which will be called in the main.py after importing this file

--- scientific.py
+++ scientific.py
@@ -1,2 +1,57 @@
 def scientific_calc():
     print("coming soon ")
+
+import math
+
+def sin(a):
+    result = math.sin(a)
+    return result
+
+def asin(a):
+    result = math.asin(a)
+    return result
+
+def cos(a):
+    result = math.cos(a)
+    return result
+
+def acos(a):
+    result = math.asin(a)
+    return result
+
+def tan(a):
+    result = math.acos(a)
+    return result
+
+def atan(a):
+    result = math.atan(a)
+    return result
+
+def log(a):
+    result = math.log(a)
+    return result
+
+def ln(a):
+    result = math.log(a) * 2.303
+    return result
+
+def power(a,b):
+    result = a ** b
+    return result
+
+def square_root(a):
+    result = math.sqrt(a)
+
+def pi():
+    result = 3.1415926535897932384626433832795028841971
+    return result
+
+def exp():
+    result = 2.7182818284590452353602874713526624977572
+    return result
+
+def factorial(a):
+    if a == 1:
+        return 1
+    else:
+        return n * factorial(a-1)
